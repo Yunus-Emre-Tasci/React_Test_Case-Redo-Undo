fd741bf16a593037499dd86fde9343442a214369
YUNUS EMRE TASCİ <yunusemretasci092@gmail.com>
2022-12-14 17:36:03 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useState } from "react"
 
 const App=()=>{
   const [points, setPoints] = useState([])
+  const [data,setData]=useState([])
 
   const clickHandle=(e)=>{
     // console.log(e.clientX,e.clientY);
@@ -10,16 +11,33 @@ const App=()=>{
       x:e.clientX,
       y:e.clientY
     }])
+    setData([])
+  }
+
+  const redoHandle=(e)=>{
+    e.stopPropagation()
+    const balls =[...points]
+    const item = balls.pop()
+    setData(data=>[...data,item])
+    setPoints(balls)
+  }
+
+  const undoHandle=(e)=>{
+    e.stopPropagation()
+    const d=[...data]
+    const item=d.pop()
+    setPoints(points=>[...points,item])
+    setData(d)
   }
 
   return(
     <div className="screen" onClick={clickHandle}>
       <header className="header">
-        <button>Redo</button>
-        <button>Undo</button>
+        <button disabled={points.length===0} onClick={redoHandle}>Redo</button>
+        <button disabled={data.length===0} onClick={undoHandle}>Undo</button>
       </header>
-      {points.map((point)=>(
-        < div className = "point"
+      {points.map((point,key)=>(
+        < div className = "point" key={key}
         style = {
           {
             left: point.x,
